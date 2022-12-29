40ae65c62ac3b894a35541a5e34648634b951bf5
TrueThanos <staletosigma@gmail.com>
2022-12-29 16:46:41 +0200

    remove unnecessary files

--- admin.js
+++
@@ -1,9 +0,0 @@
-function click_admin(){
-    $.ajax({
-        type: "POST",
-        url: "administator.php",
-    }).done(function(msg) {
-        alert("Post sent" + msg);
-    });
-    console.info("Test admin javascript");
-}
