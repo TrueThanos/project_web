98f051304e5163e181a3ef12a1ee0a708ffcffec
TrueThanos <staletosigma@gmail.com>
2023-08-15 22:26:55 +0300

    fix review
    like/dislike

--- review.php
+++ review.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     $mysqli = new mysqli("localhost","root","","customer");
 
     // Check connection{
@@ -13,16 +14,36 @@
 
     if (!$result) {
         echo("Error description: " . $mysqli -> error);
-    }          
-
+    }   
+    
+    // Check if user already reviewed
     $row = $result->fetch_assoc();
-    if (isset($_POST['action'])) {
+    $reviewers = $row['reviewers'];
+    if ($reviewers == NULL) {
+        $reviewers = "";
+    }
+
+    $email = $_SESSION["email"];
+    $user_reviewed_already = False;
+    if(strpos($reviewers, $email) !== false){
+        $user_reviewed_already = True;
+    }
+    if ($user_reviewed_already) {
+        $extra_class = " disabled";
+    } else {
+        $extra_class = " ";
+    }
+
+    $new_reviewers = $reviewers . "," .$email;
+
+    if (isset($_POST['action']) && !$user_reviewed_already) {
+        echo 'test';
         if ($_POST['action'] == 'like') {
             $new_likes = intval($row['likes']) + 1;
-            $res = $mysqli -> query("UPDATE sales SET likes=$new_likes WHERE sale_id=$sale_id");
-        } else {
+            $res = $mysqli -> query("UPDATE sales SET likes=$new_likes,reviewers='$new_reviewers' WHERE sale_id=$sale_id");
+        } else if ($_POST['action' == 'dislike']) {
             $new_dislikes = intval($row['dislikes']) + 1;
-            $res = $mysqli -> query("UPDATE sales SET dislikes=$new_dislikes WHERE sale_id=$sale_id");
+            $res = $mysqli -> query("UPDATE sales SET dislikes=$new_dislikes,reviewers='$new_reviewers' WHERE sale_id=$sale_id");
         }
     } 
 ?>
@@ -31,13 +52,15 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script type="text/javascript" src="https://code.jquery.com/jquery-1.7.1.min.js"></script>
     
-
-    <i onclick="clicklike(this)" class="fa fa-thumbs-up"></i>
-    <i onclick="clickdislike(this)" class="fa fa-thumbs-down"></i>
-
+    <?php
+    if ($user_reviewed_already) echo "User reviewed already";
+    if (!$user_reviewed_already) echo "<i onclick='clicklike(this)' class='fa fa-thumbs-up '" .$extra_class . "></i>";
+    if (!$user_reviewed_already) echo "<i onclick='clickdislike(this)' class='fa fa-thumbs-down '" .$extra_class . "></i>";
+    ?>
 
     <script>
     function clicklike(x) {
+        console.info('test clicked like');
         $.ajax({
             error: function(errMsg) {
                 console.info(errMsg);
@@ -46,7 +69,7 @@
             url: "review.php",
             data: { action: "like", sale_id: '<?php echo($sale_id)?>'},
             success: function (result) {
-                // console.log(result);
+                console.log(result);
             },
         });
         
